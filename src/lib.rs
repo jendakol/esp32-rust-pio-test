afc7841e1da44f37a50e9560adb1a85a419cf929
Jenda Kolena <jendakolena@gmail.com>
2022-08-22 13:33:27 +0200

    somehow working

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-#![no_std]
-
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    // Your implementation goes here!
-    panic!()
-}
+// #![no_std]
+//
+// use core::panic::PanicInfo;
+//
+// #[panic_handler]
+// fn panic(info: &PanicInfo) -> ! {
+//     // Your implementation goes here!
+//     panic!()
+// }
 
 // #[no_mangle]
 // #[export_name = "setup2"]
@@ -16,7 +16,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 #[export_name = "hello_from_rust"]
 extern "C" fn hello_from_rust() {
-    // println!("hi there!");
+    println!("hi there!");
 }
 
 // #[no_mangle]
