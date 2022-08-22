560959806812692a74adfab2b9e1d466ba741aac
Jenda Kolena <jendakolena@gmail.com>
2022-08-22 09:49:56 +0200

    :-(

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    // Your implementation goes here!
+    panic!()
+}
+
+// #[no_mangle]
+// #[export_name = "setup2"]
+// extern "C" fn setup2() {
+// }
+
+#[no_mangle]
+#[export_name = "hello_from_rust"]
+extern "C" fn hello_from_rust() {
+    // println!("hi there!");
+}
+
+// #[no_mangle]
+// #[export_name = "loop"]
+// extern "C" fn arduino_loop() {
+// }
